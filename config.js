522d730aed90de8767648b86e8424388669bb52c
Austin Songer <austinsonger@prevailhealth.com>
2019-07-11 16:36:14 -0500

    Initial

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "PHS PLatform Status",
+  "name": "phs_platform_status",
+  "description": "Status Page",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "toml"
+  }
+}
